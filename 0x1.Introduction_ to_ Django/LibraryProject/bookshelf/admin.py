eb7e1d21e4272e307bc5f6ecc06ce68c199985a3
Kelvin Sibuta <kelvinsibuta254@gmail.com>
2024-08-11 15:55:51 +0300

    Create a Book Library

--- 0x1.Introduction_ to_ Django/LibraryProject/bookshelf/admin.py
+++ 0x1.Introduction_ to_ Django/LibraryProject/bookshelf/admin.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 from .models import Book
 
 class BookAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', 'published_date')
+    list_filter = ('title', 'author', 'published_year')
     search_fields = ('title', 'author')
 
 # Register your models here.
