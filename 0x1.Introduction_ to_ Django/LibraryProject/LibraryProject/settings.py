b56842df91a06f6565a610ef6ce8783666f9bae2
Kelvin Sibuta <kelvinsibuta254@gmail.com>
2024-08-12 13:20:08 +0300

    Create a Book Library

--- 0x1.Introduction_ to_ Django/LibraryProject/LibraryProject/settings.py
+++ 0x1.Introduction_ to_ Django/LibraryProject/LibraryProject/settings.py
@@ -78,8 +78,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'bookshelf',
-        "USER": "root",
-        "PASSWORD": "byg6zmXp@#$%",
+        "USER": "",
+        "PASSWORD": "",
         "HOST": "127.0.0.1",
         "PORT": "3306",
     }
