27134453333bae4f0ebbb366ee17ca7ee1748459
Kelvin Sibuta <kelvinsibuta254@gmail.com>
2024-08-18 19:05:14 +0300

    Relationship App

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -14,7 +14,13 @@ from django.shortcuts import render, get_object_or_404, redirect
 from .models import Book
 from . import BookForm
 
-from django.contrib.auth.decorators import login_required, user_passes_test
+from django.shortcuts import render, redirect
+from .models import Book
+from django.views.generic.detail import DetailView
+from .models import Library
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.views import LoginView, LogoutView
 
 # Function-based View
 def list_books(request):
@@ -22,24 +28,43 @@ def list_books(request):
     return render(request, 'relationship_app/list_books.html', {'books': books})
 
 
+# Class-based View
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'relationship_app/library_detail.html'
+    context_object_name = 'library'
 
 
-from django.contrib.auth.decorators import user_passes_test
-from django.shortcuts import render
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('list_books')  # Redirect to a view after registration
+    else:
+        form = UserCreationForm()
+    return render(request, 'relationship_app/register.html', {'form': form})
 
-# Helper functions to check user roles
+
+class CustomLogoutView(LogoutView):
+    def get(self, request, *args, **kwargs):
+        return self.post(request, *args, **kwargs)
+    
+
+from django.contrib.auth.decorators import login_required, user_passes_test
+from .models import UserProfile
+
+
 def is_admin(user):
-    return user.userprofile.role == 'Admin'
+    return user.is_authenticated and user.userprofile.role == 'Admin'
 
 def is_librarian(user):
     return user.is_authenticated and user.userprofile.role == 'Librarian'
 
-def Librarian(user):
-    return user.authenticated.roles == 'Librarians'
-    # return user.userprofile.role == 'Librarian'
-
 def is_member(user):
-    return user.userprofile.role == 'Member'
+    return user.is_authenticated and user.userprofile.role == 'Member'
+
 
 # Views
 @user_passes_test(is_admin)
