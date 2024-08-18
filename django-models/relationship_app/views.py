7d2a3eabf49eebb6fc47feb6f4a7b14460a51c27
Kelvin Sibuta <kelvinsibuta254@gmail.com>
2024-08-18 17:25:07 +0300

    Relationship App

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -53,7 +53,7 @@ def Admin(request):
     return render(request, template_name='relationship_app/admin_view.html')
 
 def Librarian(user):
-    return user.is_authenticated and user.userprofile.role == "Librarians"
+    return user.is_authenticated and user.role == "Librarians"
 
 # @user_passes_test(Librarian)
 # def librarian_view(request):
