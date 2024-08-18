0a020b019da12948cba7f1c33eb4bd716025ee4b
Kelvin Sibuta <kelvinsibuta254@gmail.com>
2024-08-18 14:09:01 +0300

    Relationship App

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -32,7 +32,31 @@ class LibraryDetailView(DetailView):
 #     template_name = "books/book_update_form.html"
 #     success_url = reverse_lazy("book_list")
 
-#     def form_valid(self, form):
-#         response = super().form_valid(form)
-#         return response
+    # def form_valid(self, form):
+    #     response = super().form_valid(form)
+    #     return response
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+# relationship_app/views.py
+from django.contrib.auth.views import LoginView
+# relationship_app/views.py
+from django.contrib.auth.views import LogoutView
+
+class CustomLoginView(LoginView):
+    template_name = 'relationship_app/login.html'
+class CustomLogoutView(LogoutView):
+    template_name = 'relationship_app/logout.html'    
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')  # Adjust 'home' to your desired redirect URL
+    else:
+        form = UserCreationForm()
+    return render(request, 'relationship_app/register.html', {'form': form})
+
 
