1a61488ca21e5d49155e1616c97f4ffef983a11c
Kelvin Sibuta <kelvinsibuta254@gmail.com>
2024-08-18 09:37:59 +0300

    Added relationship app

--- django-models/relationship_app/query_samples.py
+++ django-models/relationship_app/query_samples.py
@@ -3,7 +3,7 @@ from .models import Book, Library
 #Filtering books by author
 book_by_author = Book.objects.filter(author="George Orwel")
 
-Library.objects.get(name = "name")
+Library.objects.get(name = library_name)
 
 #Listing all books
 books = Book.objects.all()
