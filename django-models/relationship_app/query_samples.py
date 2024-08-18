908380c5560142459cacca099a900ea2fdef48d6
Kelvin Sibuta <kelvinsibuta254@gmail.com>
2024-08-18 12:00:55 +0300

    Relationship App

--- django-models/relationship_app/query_samples.py
+++ django-models/relationship_app/query_samples.py
@@ -1,17 +1,17 @@
 from .models import Author, Book, Library, Librarian
 
 #Creating author field
-author = Author.objects.create(name="Wilson Rawls")
-author.save()
+Wilson = Author.objects.create(name="Wilson Rawls")
+Wilson.save()
 
 #Querying all books by a specific author
 #Wilson = Author.objects.get(name="Wilson Rawls")
-author_name = Author.objects.filter(author = author)
+author_name = Author.objects.filter(author = Wilson)
 #Adding titles of the Book
 Last = Book.objects.create(title="All the Wrong Questions: When Did you See Her Last?")
 Last.save()
 #Filtering books by author
-book_by_author = Author.objects.get(author=author_name)
+Book.objects.get(author=Wilson)
 
 #Library
 Sibuta = Library.objects.create(name="Sibuta")
