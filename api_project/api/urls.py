5198df17bd62b549ee31ea1452aaf3932a200d69
Kelvin Sibuta <kelvinsibuta254@gmail.com>
2024-09-01 15:47:31 +0300

    Add Book ViewSet and Router

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,9 +1,10 @@
 from .views import BookListAPIView, BookViewSet
-from django.urls import path
+from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
 urlpatterns = [
     path('books/', BookListAPIView.as_view(), name='book-list'),
+    path('api/', include('api.urls'))
     ]
 
 router = DefaultRouter()
