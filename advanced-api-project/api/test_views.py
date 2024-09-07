8cfc8449fd2bb30a0f9b68dab04397525d79c612
Kelvin Sibuta <kelvinsibuta254@gmail.com>
2024-09-07 18:31:21 +0300

    Implemented generic views to create advanced API

--- advanced-api-project/api/test_views.py
+++ advanced-api-project/api/test_views.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from rest_framework import status
 from rest_framework.test import APITestCase, APIClient
 from django.urls import reverse
 from django.contrib.auth.models import User
