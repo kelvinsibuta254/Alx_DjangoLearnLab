3748aaad322dacc3f6754c1c2b814a79f58647aa
Kelvin Sibuta <kelvinsibuta254@gmail.com>
2024-09-14 13:23:55 +0300

    blog app

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -83,8 +83,12 @@ WSGI_APPLICATION = 'django_blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'members',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': 3306
     }
 }
 
