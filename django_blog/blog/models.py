5448c2ba4ea0eab30f6bc971a13713fa086c39ac
Kelvin Sibuta <kelvinsibuta254@gmail.com>
2024-09-14 13:56:34 +0300

    blog app

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -29,3 +29,10 @@ class Post(models.Model):
     content = models.TextField()
     published_date = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class Profile(models.model):
+    pic = models.URLField()
+    bio = models.TextField(max_length=255)
+    address = models.CharField(max_length=100)
+    country = models.CharField(max_length=50)
+    user = models.OneToOneField(User, on_delete = models.CASCADE, null=True, blank=True)
--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -4,8 +4,8 @@ from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path("", index, name="index"),
-    path("register", register, name="register"),
-    path("login", LoginView.as_view(next_page = "product"), name="login"),
-    path("product", product, name="product"),
-    path("logout", LogoutView.as_view(next_page="/"), name="logout")
+    path("register/", register, name="register"),
+    path("login/", LoginView.as_view(), name="login"),
+    path("profile", product, name="profile"),
+    path("logout/", LogoutView.as_view(next_page="/"), name="logout")
 ]
