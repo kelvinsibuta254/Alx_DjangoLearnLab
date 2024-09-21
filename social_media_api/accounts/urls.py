da82a29dddc78bd42fd5097dcb1da93a9d8428a7
Kelvin Sibuta <kelvinsibuta254@gmail.com>
2024-09-21 12:24:41 +0300

    Set Up a social media project and Implemented User Authentication

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 
-from .views import UserRegisterView, LoginView, LogoutView, ProfileUpdateView
+from .views import UserRegisterView, LoginView, LogoutView, ProfileUpdateView, FollowUserView, UnfollowUserView
 
 urlpatterns = [
     path('register/', UserRegisterView.as_view()),
     path('login/', LoginView.as_view()),
     path('profile', ProfileUpdateView.as_view()),
     path('logout/', LogoutView.as_view()),
+    path('follow/<int:user_id>/', FollowUserView.as_view()),
+    path('unfollow/<int:user_id>/', UnfollowUserView.as_view())
+
 ]
