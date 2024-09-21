0141b77d5c656401b863f1b060f0b3cdd5510a71
Kelvin Sibuta <kelvinsibuta254@gmail.com>
2024-09-21 13:00:50 +0300

    Set Up a social media project and Implemented User Authentication

--- social_media_api/notifications/models.py
+++ social_media_api/notifications/models.py
@@ -1,18 +1,36 @@
+# from django.db import models
+# from django.conf import settings
+# from django.contrib.contenttypes.models import ContentType
+# from django.contrib.contenttypes.fields import GenericForeignKey
+# # Create your models here.
+# class Notification(models.Model):
+#     recipient = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='notification_receiver')
+#     actor = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='notification_sender')
+#     verb = [
+#         ('Follow', 'notification on new following'),
+#         ('Post', 'notification on new post'),
+#         ('Like', 'notification on new post like'),
+#         ('Comment', 'notification on post comments'),
+#     ]
+#     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+#     object_id = models.PositiveIntegerField()
+#     target = GenericForeignKey('content_type', 'object_id')
+#     timestamp = models.DateField(auto_now=True)
+
+
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.auth.models import User
 from django.db import models
 from django.conf import settings
-from django.contrib.contenttypes.models import ContentType
-from django.contrib.contenttypes.fields import GenericForeignKey
-# Create your models here.
+
+
 class Notification(models.Model):
-    recipient = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='notification_receiver')
-    actor = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='notification_sender')
-    verb = [
-        ('Follow', 'notification on new following'),
-        ('Post', 'notification on new post'),
-        ('Like', 'notification on new post like'),
-        ('Comment', 'notification on post comments'),
-    ]
-    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
-    object_id = models.PositiveIntegerField()
-    target = GenericForeignKey('content_type', 'object_id')
-    timestamp = models.DateField(auto_now=True)
+    recipient = models.ForeignKey(User, on_delete=models.CASCADE, related_name='notifications')
+    actor = models.ForeignKey(User, on_delete=models.CASCADE, related_name='actor')
+    verb = models.CharField(max_length=255)
+    target_content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    target_object_id = models.PositiveIntegerField()
+    target = GenericForeignKey('target_content_type', 'target_object_id')
+    timestamp = models.DateTimeField(auto_now_add=True)
+    read = models.BooleanField(default=False)
